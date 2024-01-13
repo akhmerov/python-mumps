2bbdf0d5bb23cfe6df725546a43a1a1ca0a9e0b0
Anton Akhmerov <anton.akhmerov@gmail.com>
2024-01-13 22:17:52 +0100

    fix version from git

--- mumpy/__init__.py
+++ mumpy/__init__.py
@@ -40,7 +40,7 @@ try:
     # developers.
     from setuptools_scm import get_version
 
-    __version__ = get_version()
+    __version__ = get_version(root="..", relative_to=__file__)
 except (ImportError, LookupError):
     from importlib.metadata import version, PackageNotFoundError
 
